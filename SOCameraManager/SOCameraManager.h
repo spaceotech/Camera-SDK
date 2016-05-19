2f2e3e98de5a57bcc5b9960e77175cc41433e500
Chirag <chiragl.spaceo@gmail.com>
2016-05-19 14:40:23 +0530

    Upload custom camera manager.

---
+++ SOCameraManager/SOCameraManager.h
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOCameraManager.
+FOUNDATION_EXPORT double SOCameraManagerVersionNumber;
+
+//! Project version string for SOCameraManager.
+FOUNDATION_EXPORT const unsigned char SOCameraManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOCameraManager/PublicHeader.h>
+
